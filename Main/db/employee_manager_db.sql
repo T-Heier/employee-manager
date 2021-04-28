e629069fe7e89f75171016458f0556290b9b8238
T Heier <trentheier@gmail.com>
2021-04-27 20:32:42 -0400

    created new seed file and redid the tables

--- Main/db/employee_manager_db.sql
+++ Main/db/employee_manager_db.sql
@@ -7,18 +7,18 @@ create table employee (
     id int AUTO_INCREMENT not null,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
-    role_id int,
-    manager_id int,
+    primary key (id)
 )
 
 create table role (
     id int AUTO_INCREMENT not null,
     title varchar(30),
     salary decimal,
-    department_id int,
+    primary key (id)
 )
 
 create table department (
     id int auto_increment not null,
     name varchar(30) not null,
+    primary key (id)
 )
