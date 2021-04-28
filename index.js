2100338534146afaef418f3123ec75a6ad4e9eea
T Heier <trentheier@gmail.com>
2021-04-27 21:31:44 -0400

    created the first create department function query

---
+++ index.js
@@ -0,0 +1,90 @@
+const mysqul = require('mysql');
+const inquirer = require('inquirer');
+const { runInContext } = require('node:vm');
+
+const connetion = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: 'root',
+    database: 'employee_manager_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    startCreation()
+})
+
+const startCreation = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View departments',
+                'View employees',
+                'View employee roles',
+                'Create a department',
+                'Create employee role',
+                'Create employee',
+                'Update employee role',
+            ],
+        })
+        // switch statement to pick which function you would like to run from the inquirer prompt
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View departments':
+                    viewDepartments();
+                    break;
+                case 'View employees':
+                    viewEmployee();
+                    break;
+                case 'View employee roles':
+                    viewRoles();
+                    break;
+                case 'Create a department':
+                    createDepartment();
+                    break;
+                case 'Create employee role':
+                    createEmployeeRole();
+                    break;
+                case 'Create employee':
+                    createEmployee();
+                    break;
+                case 'Update employee role':
+                    updateRole();
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        });
+};
+
+const createDepartment = () => {
+    // inquirer prompt to create and name your new department
+    inquirer
+    .prompt({
+        name: 'department',
+        type: 'input',
+        message: 'What is the department you are wanting to create',
+    })
+    .then((answer) => {
+        connection.query({
+            name: answer.department
+        },
+        (err) => {
+            if (err) throw err;
+            console.log(`You have successfully created ${answer.name} department`);
+            startCreation();
+            }
+        );
+    });
+};
+
