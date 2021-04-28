1b36d04b2cc0cca69ffc1583c85ea7db68a263ca
T Heier <trentheier@gmail.com>
2021-04-27 20:12:33 -0400

    initial commit + creating database and tables

---
+++ Main/db/employee_manager_db.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_manager_DB;
+CREATE database employee_manager_DB;
+
+USE employee_manager_DB
+
+create table employee (
+    id int AUTO_INCREMENT not null,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int,
+    manager_id int,
+)
+
+create table role (
+    id int AUTO_INCREMENT not null,
+    title varchar(30),
+    salary decimal,
+    department_id int,
+)
+
+create table department (
+    id int auto_increment not null,
+    name varchar(30) not null,
+)
